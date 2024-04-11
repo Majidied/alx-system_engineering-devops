147e603f58f6574c59cfc362d4ce6b371034a2eb
majidied <majidimajidi2003@gmail.com>
2024-04-11 17:02:22 +0000

    Refactor API advanced script to use f-string for URL formatting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,17 @@
-#!/usr/bin/python3
-"""Module to query number of subscribers of a subreddit"""
 import requests
+
+#!/usr/bin/python3
+"""
+Module to query number of subscribers of a subreddit
+"""
+
 
 
 def number_of_subscribers(subreddit):
-    """Function to query number of subscribers of a subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """
+    Function to query number of subscribers of a subreddit
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
