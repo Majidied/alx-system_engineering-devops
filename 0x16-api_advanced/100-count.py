a42a575ed2c824fadb590ca96991dc5e3360b675
majidied <majidimajidi2003@gmail.com>
2024-04-11 17:01:25 +0000

    Refactor count_words function in 0x16-api_advanced/100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,25 @@
-#!/usr/bin/python3
-"""Module to query number of subscribers of a subreddit"""
 import requests
 
+#!/usr/bin/python3
+"""Module to query number of subscribers of a subreddit."""
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Function to query number of subscribers of a subreddit"""
+
+def count_words(subreddit, word_list, word_count=None, after=None):
+    """Function to query number of subscribers of a subreddit."""
+    if word_count is None:
+        word_count = {}
+
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
         subreddit, after)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     response.raise_for_status()
-    if response.status_code != 200:
+
+    if response.status_code is not 200:
         return None
+
     after = response.json().get('data').get('after')
+
     if after is None:
         for i in response.json().get('data').get('children'):
             title = i.get('data').get('title')
@@ -22,11 +29,14 @@ def count_words(subreddit, word_list, word_count={}, after=None):
                         word_count[word] += 1
                     else:
                         word_count[word] = 1
+
         if len(word_count) == 0:
             return
+
         for key, value in sorted(word_count.items(), key=lambda x: x[1], reverse=True):
             print('{}: {}'.format(key, value))
         return
+
     for i in response.json().get('data').get('children'):
         title = i.get('data').get('title')
         for word in word_list:
@@ -35,4 +45,5 @@ def count_words(subreddit, word_list, word_count={}, after=None):
                     word_count[word] += 1
                 else:
                     word_count[word] = 1
+
     return count_words(subreddit, word_list, word_count, after)
