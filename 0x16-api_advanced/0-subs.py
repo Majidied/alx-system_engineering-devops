216fb0a4c68207d6538e17f0d2b650864178c7ae
majidied <majidimajidi2003@gmail.com>
2024-04-11 17:04:04 +0000

    Refactor shebang and import statements in 0x16-api_advanced/100-count.py and 0x16-api_advanced/0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
+"""Module to query number of subscribers of a subreddit"""
 import requests
-
-"""
-Module to query number of subscribers of a subreddit
-"""
-
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function to query number of subscribers of a subreddit
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    """Function to query number of subscribers of a subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
